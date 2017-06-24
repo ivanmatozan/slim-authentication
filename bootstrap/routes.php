b9dd639eb8bb8eb87591dfb2545b76ce437d0c32
Ivan Matozan <ivan.matozan@gmail.com>
2017-06-24 21:11:05 +0200

    Protect routes with middleware

--- bootstrap/routes.php
+++ bootstrap/routes.php
@@ -2,13 +2,17 @@
 
 $app->get('/', 'HomeController:index')->setName('home');
 
-$app->get('/signup', 'AuthController:getSignUp')->setName('auth.signup');
-$app->post('/signup', 'AuthController:postSignUp');
+$app->group('', function () {
+    $this->get('/signup', 'AuthController:getSignUp')->setName('auth.signup');
+    $this->post('/signup', 'AuthController:postSignUp');
 
-$app->get('/signin', 'AuthController:getSignIn')->setName('auth.signin');
-$app->post('/signin', 'AuthController:postSignIn');
+    $this->get('/signin', 'AuthController:getSignIn')->setName('auth.signin');
+    $this->post('/signin', 'AuthController:postSignIn');
+})->add(new \App\Middleware\GuestMiddleware($container));
 
-$app->get('/signout', 'AuthController:getSignOut')->setName('auth.signout');
+$app->group('', function () {
+    $this->get('/signout', 'AuthController:getSignOut')->setName('auth.signout');
 
-$app->get('/password/change', 'PasswordController:getChangePassword')->setName('auth.password.change');
-$app->post('/password/change', 'PasswordController:postChangePassword');
+    $this->get('/password/change', 'PasswordController:getChangePassword')->setName('auth.password.change');
+    $this->post('/password/change', 'PasswordController:postChangePassword');
+})->add(new \App\Middleware\AuthMiddleware($container));
