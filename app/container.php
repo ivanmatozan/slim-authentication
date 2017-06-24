5c0513eb1a16fac65424bddc9521381ba88a9e66
Ivan Matozan <ivan.matozan@gmail.com>
2017-06-24 10:35:48 +0200

    Setup Twig

--- app/container.php
+++ app/container.php
@@ -1,3 +1,15 @@
 <?php
 
 $container = $app->getContainer();
+
+// Twig
+$container['view'] = function ($container) {
+    $twig = new \Slim\Views\Twig(__DIR__ . '/../resources/views');
+
+    $twig->addExtension(new \Slim\Views\TwigExtension(
+        $container->router,
+        $container->request->getUri()
+    ));
+
+    return $twig;
+};
